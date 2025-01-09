e94e047a9e933d19fc69e60bcbda095680ca5209
Junior <eecfredes@gmail.com>
2025-01-09 20:22:13 -0300

    :rocket: List account

--- db/query/account.sql
+++ db/query/account.sql
@@ -10,3 +10,10 @@ INSERT INTO accounts (
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+WHERE owner = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
