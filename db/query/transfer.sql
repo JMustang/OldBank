f135a2773c38885e3c5b5954233cdcfd772bea49
Junior <eecfredes@gmail.com>
2025-01-10 21:54:30 -0300

    :rocket: List transfer

--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -1,7 +1,7 @@
 -- name: CreateTransfer :one
 INSERT INTO transfers (
-    from_accoubt_id,
-    to_accoubt_id,
+    from_account_id,
+    to_account_id,
     amount
 ) VALUES (
     $1, $2, $3
@@ -10,3 +10,12 @@ INSERT INTO transfers (
 -- name: GetTransfer :one
 SELECT * FROM transfers
 WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE
+    from_account_id = $1 OR
+    to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
