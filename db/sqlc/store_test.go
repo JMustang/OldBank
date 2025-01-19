6cfbc84a94aa8fa3c6a464946b4601bf6b47b482
Junior <eecfredes@gmail.com>
2025-01-19 16:27:48 -0300

    :white_check_mark:Store test, done

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.account.ID,
+				ToAccountID:   account2.account.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	for i := 0; i < 0; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.account.ID, transfer.FromAccountID)
+		require.Equal(t, account2.account.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.account.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		ToEntry := result.ToEntry
+		require.NotEmpty(t, ToEntry)
+		require.Equal(t, account2.account.ID, ToEntry.AccountID)
+		require.Equal(t, amount, ToEntry.Amount)
+		require.NotZero(t, ToEntry.ID)
+		require.NotZero(t, ToEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), ToEntry.ID)
+		require.NoError(t, err)
+
+		// TODO: check account balances
+	}
+}
