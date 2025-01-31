0fd95c9dbb988187254e5a2786c3862d60fe3a84
Junior <eecfredes@gmail.com>
2025-01-30 21:57:26 -0300

    :rocket:Using viper config on main file

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/JMustang/OldBank/util"
+
 	"github.com/JMustang/OldBank/api"
 	db "github.com/JMustang/OldBank/db/sqlc"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5433/OldBank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
