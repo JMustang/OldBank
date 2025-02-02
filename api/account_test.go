20ec6545e961c0b9b5a67192ab0e89a8f3cb310d
Junior <eecfredes@gmail.com>
2025-02-01 22:09:48 -0300

    :white_check_mark: Test get account with mock

--- api/account_test.go
+++ api/account_test.go
@@ -1,7 +1,47 @@
 package api
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/JMustang/OldBank/db/mock"
+	db "github.com/JMustang/OldBank/db/sqlc"
+	"github.com/JMustang/OldBank/util"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
 
 func TestGetAccountAPI(t *testing.T) {
-	// test code
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+
+	server := NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recorder, request)
+
+	require.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
 }
