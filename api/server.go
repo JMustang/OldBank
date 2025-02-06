ba9a9037d8fff580719080d609196312f49f81af
Junior <eecfredes@gmail.com>
2025-02-06 20:17:44 -0300

    :rocket: validation engine

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/JMustang/OldBank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests for our banking servoce.
@@ -15,6 +17,10 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	// Add router
 	router.POST("/accounts", server.createAccount)
