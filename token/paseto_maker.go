4578547ac01d5a5551af15781116e5275a30f214
Junior <eecfredes@gmail.com>
2025-03-02 20:57:11 -0300

    :rocket:Verify token with paseto

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -36,3 +36,20 @@ func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (
 
 	return maker.paseto.Encrypt(maker.symmetricKey, Payload, nil)
 }
+
+// VerifyToken checks if the token is valid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
