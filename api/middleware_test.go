65927c4a6675731a60578d5305b0cba65dfe022f
Junior <eecfredes@gmail.com>
2025-03-10 22:17:27 -0300

    :white_check_mark:Authorization function

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,21 +1,46 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/JMustang/OldBank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 )
 
+func addAuthorization(
+	t *testing.T,
+	request *http.Request,
+	tokenMaker token.Maker,
+	authorizationType string,
+	username string,
+	duration time.Duration,
+) {
+	token, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+
+	authorizationHeader := fmt.Sprintf("%s %s", authorizationType, token)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
+}
+
 func TestAuthMiddleware(t *testing.T) {
 	testCases := []struct {
 		name          string
 		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
 		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
-	}{}
+	}{
+		name: "OK",
+		setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+			addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "username", role, time.Minute)
+		},
+		checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			require.Equal(t, http.StatusOK, recorder.Code)
+		},
+	}
 
 	for i := range testCases {
 		tc := testCases[i]
