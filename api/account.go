8d4a430f7f5cf5aad5b8e8bdb50505008acc22a2
Junior <eecfredes@gmail.com>
2025-03-12 22:04:43 -0300

    :rocket: adding an auth payload to account users

--- api/account.go
+++ api/account.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 
 	db "github.com/JMustang/OldBank/db/sqlc"
+	"github.com/JMustang/OldBank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +21,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
